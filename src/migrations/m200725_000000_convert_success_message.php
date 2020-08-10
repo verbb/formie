86318a4e74ac8c0381f0960d986cfecff1c0efa3
Jayden Smith <jayden@sgroup.com.au>
2020-08-10 11:10:39 +1000

    Fixed potentially problematic migrations.

--- src/migrations/m200725_000000_convert_success_message.php
+++ src/migrations/m200725_000000_convert_success_message.php
@@ -26,36 +26,37 @@ class m200725_000000_convert_success_message extends Migration
         foreach ($forms as $form) {
             $settings = Json::decode($form['settings']);
 
-            $submitActionMessage = (new Renderer)->render('<p>' . $settings['submitActionMessage'] . '</p>');
-            $settings['submitActionMessage'] = $submitActionMessage['content'];
-            
-            $this->db->createCommand()
-                ->update('{{%formie_forms}}', [
-                    'settings' => Json::encode($settings),
-                ], ['id' => $form['id']])
-                ->execute();
+            if (!is_array($settings['submitActionMessage'])) {
+                $submitActionMessage = (new Renderer)->render('<p>' . $settings['submitActionMessage'] . '</p>');
+                $settings['submitActionMessage'] = $submitActionMessage['content'];
+
+                $this->db->createCommand()
+                    ->update('{{%formie_forms}}', [
+                        'settings' => Json::encode($settings),
+                    ], ['id' => $form['id']])
+                    ->execute();
+            }
         }
 
         // Don't make the same config changes twice
         $projectConfig = Craft::$app->getProjectConfig();
         $schemaVersion = $projectConfig->get('plugins.formie.schemaVersion', true);
-        
+
         if (version_compare($schemaVersion, '1.0.2', '>=')) {
             return;
         }
 
-        // Update the project config for schemas
-        $stencils = $projectConfig->get('formie.stencils');
+        // Update the project config for stencils.
+        if ($stencils = $projectConfig->get('formie.stencils')) {
+            foreach ($stencils as $key => $stencil) {
+                $data = $stencil['data'];
 
-        foreach ($stencils as $key => $stencil) {
-            $data = $stencil['data'];
-            
-            $submitActionMessage = (new Renderer)->render('<p>' . $data['settings']['submitActionMessage'] . '</p>');
-            $data['settings']['submitActionMessage'] = Json::encode($submitActionMessage['content']);
+                $submitActionMessage = (new Renderer)->render('<p>' . $data['settings']['submitActionMessage'] . '</p>');
+                $data['settings']['submitActionMessage'] = Json::encode($submitActionMessage['content']);
 
-            $projectConfig->set('formie.stencils.' . $key . '.data', $data);
+                $projectConfig->set('formie.stencils.' . $key . '.data', $data);
+            }
         }
-
     }
 
     /**
--- src/migrations/m200725_100000_convert_error_message.php
+++ src/migrations/m200725_100000_convert_error_message.php
@@ -26,36 +26,37 @@ class m200725_100000_convert_error_message extends Migration
         foreach ($forms as $form) {
             $settings = Json::decode($form['settings']);
 
-            $errorMessage = (new Renderer)->render('<p>' . $settings['errorMessage'] . '</p>');
-            $settings['errorMessage'] = $errorMessage['content'];
-            
-            $this->db->createCommand()
-                ->update('{{%formie_forms}}', [
-                    'settings' => Json::encode($settings),
-                ], ['id' => $form['id']])
-                ->execute();
+            if (!is_array($settings['errorMessage'])) {
+                $errorMessage = (new Renderer)->render('<p>' . $settings['errorMessage'] . '</p>');
+                $settings['errorMessage'] = $errorMessage['content'];
+
+                $this->db->createCommand()
+                    ->update('{{%formie_forms}}', [
+                        'settings' => Json::encode($settings),
+                    ], ['id' => $form['id']])
+                    ->execute();
+            }
         }
 
         // Don't make the same config changes twice
         $projectConfig = Craft::$app->getProjectConfig();
         $schemaVersion = $projectConfig->get('plugins.formie.schemaVersion', true);
-        
+
         if (version_compare($schemaVersion, '1.0.3', '>=')) {
             return;
         }
 
-        // Update the project config for schemas
-        $stencils = $projectConfig->get('formie.stencils');
+        // Update the project config for stencils.
+        if ($stencils = $projectConfig->get('formie.stencils')) {
+            foreach ($stencils as $key => $stencil) {
+                $data = $stencil['data'];
 
-        foreach ($stencils as $key => $stencil) {
-            $data = $stencil['data'];
-            
-            $errorMessage = (new Renderer)->render('<p>' . $data['settings']['errorMessage'] . '</p>');
-            $data['settings']['errorMessage'] = Json::encode($errorMessage['content']);
+                $errorMessage = (new Renderer)->render('<p>' . $data['settings']['errorMessage'] . '</p>');
+                $data['settings']['errorMessage'] = Json::encode($errorMessage['content']);
 
-            $projectConfig->set('formie.stencils.' . $key . '.data', $data);
+                $projectConfig->set('formie.stencils.' . $key . '.data', $data);
+            }
         }
-
     }
 
     /**
