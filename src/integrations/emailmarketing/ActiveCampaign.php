26ce08e76d3f9050bae109846adb000592ed5679
Josh Crawford <joshua@sgroup.com.au>
2020-08-18 23:31:03 +1000

    ActiveCampaign

--- src/integrations/emailmarketing/ActiveCampaign.php
+++ src/integrations/emailmarketing/ActiveCampaign.php
@@ -21,8 +21,6 @@ class ActiveCampaign extends EmailMarketing
     // =========================================================================
 
     public $handle = 'activeCampaign';
-    public $listId;
-    public $fieldMapping;
 
 
     // Public Methods
@@ -81,9 +79,15 @@ class ActiveCampaign extends EmailMarketing
     {
         if ($this->enabled) {
             $apiKey = $this->settings['apiKey'] ?? '';
+            $apiUrl = $this->settings['apiUrl'] ?? '';
 
             if (!$apiKey) {
                 $this->addError('apiKey', Craft::t('formie', 'API key is required.'));
+                return false;
+            }
+
+            if (!$apiUrl) {
+                $this->addError('apiUrl', Craft::t('formie', 'API URL is required.'));
                 return false;
             }
         }
@@ -99,6 +103,76 @@ class ActiveCampaign extends EmailMarketing
         $allLists = [];
 
         try {
+            $response = $this->_request('GET', 'lists', [
+                'query' => [
+                    'limit' => 100,
+                ],
+            ]);
+
+            $lists = $response['lists'] ?? [];
+
+            foreach ($lists as $list) {
+                // While we're at it, fetch the fields for the list
+                $response = $this->_request('GET', 'fields', [
+                    'query' => [
+                        'limit' => 100,
+                    ],
+                ]);
+
+                $fields = $response['fields'] ?? [];
+
+                $listFields = [
+                    new EmailMarketingField([
+                        'tag' => 'email',
+                        'name' => Craft::t('formie', 'Email'),
+                        'type' => 'email',
+                        'required' => true,
+                    ]),
+                    new EmailMarketingField([
+                        'tag' => 'firstName',
+                        'name' => Craft::t('formie', 'First Name'),
+                        'type' => 'firstName',
+                    ]),
+                    new EmailMarketingField([
+                        'tag' => 'lastName',
+                        'name' => Craft::t('formie', 'Last Name'),
+                        'type' => 'lastName',
+                    ]),
+                    new EmailMarketingField([
+                        'tag' => 'phone',
+                        'name' => Craft::t('formie', 'Phone'),
+                        'type' => 'phone',
+                    ]),
+                ];
+
+                // Don't use all fields, at least for the moment...
+                $supportedFields = [
+                    'text',
+                    'textarea',
+                    'hidden',
+                    'dropdown',
+                    'radio',
+                    'date',
+                    // 'checkbox',
+                    // 'listbox',
+                ];
+
+                foreach ($fields as $field) {
+                    if (in_array($field['type'], $supportedFields)) {
+                        $listFields[] = new EmailMarketingField([
+                            'tag' => $field['id'],
+                            'name' => $field['title'],
+                            'type' => $field['type'],
+                        ]);
+                    }
+                }
+
+                $allLists[] = new EmailMarketingList([
+                    'id' => $list['id'],
+                    'name' => $list['name'],
+                    'fields' => $listFields,
+                ]);
+            }
             
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
@@ -117,7 +191,71 @@ class ActiveCampaign extends EmailMarketing
     public function sendPayload(Submission $submission): bool
     {
         try {
-            
+            $fieldValues = $this->getFieldMappingValues($submission);
+
+            // Pull out email, as it needs to be top level
+            $email = ArrayHelper::remove($fieldValues, 'email');
+            $firstName = ArrayHelper::remove($fieldValues, 'firstName');
+            $lastName = ArrayHelper::remove($fieldValues, 'lastName');
+            $phone = ArrayHelper::remove($fieldValues, 'phone');
+
+            $payload = [
+                'contact' => [
+                    'email' => $email,
+                    'first_name' => $firstName,
+                    'last_name' => $lastName,
+                    'phone' => $phone,
+                ],
+            ];
+
+            // Allow events to cancel sending
+            if (!$this->beforeSendPayload($submission)) {
+                return false;
+            }
+
+            // Create or update contact
+            $response = $this->_request('POST', 'contact/sync', [
+                'json' => $payload,
+            ]);
+
+            $contactId = $response['contact']['id'] ?? '';
+
+            if (!$contactId) {
+                Integration::error($this, Craft::t('formie', 'Missing return “contactId” {response}', [
+                    'response' => Json::encode($response),
+                ]));
+
+                return false;
+            }
+
+            // Then add them to the list
+            $response = $this->_request('POST', 'contactLists', [
+                'json' => [
+                    'contactList' => [
+                        'list' => $this->listId,
+                        'contact' => $contactId,
+                        'status' => 1,
+                    ],
+                ],
+            ]);
+
+            // Then finally sort out the custom fields, annoyingly, one at a time
+            foreach ($fieldValues as $key => $value) {
+                $response = $this->_request('POST', 'fieldValues', [
+                    'json' => [
+                        'fieldValue' => [
+                            'contact' => $contactId,
+                            'field' => $key,
+                            'value' => $value,
+                        ],
+                    ],
+                ]);
+            }
+
+            // Allow events to say the response is invalid
+            if (!$this->afterSendPayload($submission, $response)) {
+                return false;
+            }
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
                 'message' => $e->getMessage(),
@@ -137,7 +275,21 @@ class ActiveCampaign extends EmailMarketing
     public function fetchConnection(): bool
     {
         try {
-            
+            $clientId = $this->settings['clientId'] ?? '';
+
+            $response = $this->_request('GET', 'lists');
+            $error = $response['error'] ?? '';
+            $lists = $response['lists'] ?? '';
+
+            if ($error) {
+                Integration::error($this, $error, true);
+                return false;
+            }
+
+            if (!$lists) {
+                Integration::error($this, 'Unable to find “{lists}” in response.', true);
+                return false;
+            }
 
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
@@ -163,14 +315,19 @@ class ActiveCampaign extends EmailMarketing
         }
 
         $apiKey = $this->settings['apiKey'] ?? '';
+        $apiUrl = $this->settings['apiUrl'] ?? '';
 
         if (!$apiKey) {
-            Integration::error($this, 'Invalid API Key for Mailchimp', true);
+            Integration::error($this, 'Invalid API Key for Active Campaign', true);
+        }
+
+        if (!$apiUrl) {
+            Integration::error($this, 'Invalid API URL for Active Campaign', true);
         }
 
         return $this->_client = Craft::createGuzzleClient([
-            // 'base_uri' => 'https://' . $dataCenter . '.api.mailchimp.com/3.0/',
-            // 'auth' => ['apikey', $apiKey],
+            'base_uri' => trim($apiUrl, '/') . '/api/3/',
+            'headers' => ['Api-Token' => $apiKey],
         ]);
     }
 
