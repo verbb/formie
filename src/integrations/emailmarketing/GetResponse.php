8a18a9b8446f550be02da0f3cec69b151c353373
Josh Crawford <joshua@sgroup.com.au>
2020-08-19 15:46:55 +1000

    GetResponse

--- src/integrations/emailmarketing/GetResponse.php
+++ src/integrations/emailmarketing/GetResponse.php
@@ -65,7 +65,40 @@ class GetResponse extends EmailMarketing
     public function fetchLists()
     {
         $allLists = [];
+            $lists = $this->_request('GET', 'campaigns');
 
+            foreach ($lists as $list) {
+                // While we're at it, fetch the fields for the list
+                $fields = $this->_request('GET', 'custom-fields');
+
+                $listFields = [
+                    new EmailMarketingField([
+                        'tag' => 'email',
+                        'name' => Craft::t('formie', 'Email'),
+                        'type' => 'email',
+                        'required' => true,
+                    ]),
+                    new EmailMarketingField([
+                        'tag' => 'name',
+                        'name' => Craft::t('formie', 'Name'),
+                        'type' => 'name',
+                    ]),
+                ];
+            
+                foreach ($fields as $field) {
+                    $listFields[] = new EmailMarketingField([
+                        'tag' => $field['customFieldId'],
+                        'name' => $field['name'],
+                        'type' => $field['fieldType'],
+                    ]);
+                }
+
+                $allLists[] = new EmailMarketingList([
+                    'id' => $list['campaignId'],
+                    'name' => $list['name'],
+                    'fields' => $listFields,
+                ]);
+            }
         try {
             
         } catch (\Throwable $e) {
@@ -85,7 +118,54 @@ class GetResponse extends EmailMarketing
     public function sendPayload(Submission $submission): bool
     {
         try {
-            
+            $fieldValues = $this->getFieldMappingValues($submission);
+
+            // Pull out email, as it needs to be top level
+            $email = ArrayHelper::remove($fieldValues, 'email');
+            $name = ArrayHelper::remove($fieldValues, 'name');
+
+            // Format custom fields
+            $customFields = [];
+
+            foreach ($fieldValues as $key => $value) {
+                if (is_array($value)) {
+                    foreach ($value as $v) {
+                        $customFields[] = [
+                            'customFieldId' => $key,
+                            'value' => [$v],
+                        ];
+                    }
+                } else {
+                    $customFields[] = [
+                        'customFieldId' => $key,
+                        'value' => [$value],
+                    ];
+                }
+            }
+
+            $payload = [
+                'email' => $email,
+                'name' => $name,
+                'campaign' => [
+                    'campaignId' => $this->listId,
+                ],
+                'customFieldValues' => $customFields,
+            ];
+
+            // Allow events to cancel sending
+            if (!$this->beforeSendPayload($submission, $payload)) {
+                return false;
+            }
+
+            // Add or update
+            $response = $this->_request('POST', 'contacts', [
+                'json' => $payload,
+            ]);
+
+            // Allow events to say the response is invalid
+            if (!$this->afterSendPayload($submission, $payload, $response)) {
+                return false;
+            }
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
                 'message' => $e->getMessage(),
@@ -105,7 +185,13 @@ class GetResponse extends EmailMarketing
     public function fetchConnection(): bool
     {
         try {
-            
+            $response = $this->_request('GET', 'accounts');
+            $accountId = $response['accountId'] ?? '';
+
+            if (!$accountId) {
+                Integration::error($this, 'Unable to find “{accountId}” in response.', true);
+                return false;
+            }
 
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
@@ -137,8 +223,8 @@ class GetResponse extends EmailMarketing
         }
 
         return $this->_client = Craft::createGuzzleClient([
-            // 'base_uri' => 'https://' . $dataCenter . '.api.mailchimp.com/3.0/',
-            // 'auth' => ['apikey', $apiKey],
+            'base_uri' => 'https://api.getresponse.com/v3/',
+            'headers' => ['X-Auth-Token' => 'api-key ' . $apiKey],
         ]);
     }
 
