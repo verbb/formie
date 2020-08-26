cd95fd29fc326d0795083b7f0a45179ee726b831
Josh Crawford <joshua@sgroup.com.au>
2020-08-27 01:13:35 +1000

    Pipeliner

--- src/integrations/crm/Pipeliner.php
+++ src/integrations/crm/Pipeliner.php
@@ -20,7 +20,12 @@ class Pipeliner extends Crm
     // Properties
     // =========================================================================
 
-    public $apiKey;
+    public $apiToken;
+    public $apiPassword;
+    public $apiSpaceId;
+    public $apiServiceUrl;
+    public $mapToContact = false;
+    public $contactFieldMapping;
 
 
     // Public Methods
@@ -49,7 +54,14 @@ class Pipeliner extends Crm
     {
         $rules = parent::defineRules();
 
-        $rules[] = [['apiKey'], 'required'];
+        $rules[] = [['apiToken', 'apiPassword', 'apiSpaceId', 'apiServiceUrl'], 'required'];
+
+        $contact = $this->getFormSettings()['contact'] ?? [];
+
+        // Validate the following when saving form settings
+        $rules[] = [['contactFieldMapping'], 'validateFieldMapping', 'params' => $contact, 'when' => function($model) {
+            return $model->enabled && $model->mapToContact;
+        }, 'on' => [Integration::SCENARIO_FORM]];
 
         return $rules;
     }
@@ -59,10 +71,110 @@ class Pipeliner extends Crm
      */
     public function fetchFormSettings()
     {
-        $allLists = [];
+        $settings = [];
 
         try {
-            
+            $response = $this->request('GET', 'entities/Clients');
+            $clients = $response['data'] ?? [];
+
+            $clientOptions = [];
+
+            foreach ($clients as $key => $client) {
+                $clientOptions[] = [
+                    'label' => $client['name'],
+                    'value' => $client['id'],
+                ];
+            }
+
+            $contactFields = [
+                new IntegrationField([
+                    'handle' => 'owner_id',
+                    'name' => Craft::t('formie', 'Owner ID'),
+                    'required' => true,
+                    'options' => [
+                        'label' => Craft::t('formie', 'Lists'),
+                        'options' => $clientOptions,
+                    ],
+                ]),
+                new IntegrationField([
+                    'handle' => 'address',
+                    'name' => Craft::t('formie', 'Address'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'city',
+                    'name' => Craft::t('formie', 'City'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'state_province',
+                    'name' => Craft::t('formie', 'State Province'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'zip_code',
+                    'name' => Craft::t('formie', 'Zip Code'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'country',
+                    'name' => Craft::t('formie', 'Country'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'comments',
+                    'name' => Craft::t('formie', 'Comments'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'contact_type_id',
+                    'name' => Craft::t('formie', 'Contact Type ID'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'email1',
+                    'name' => Craft::t('formie', 'Email'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'first_name',
+                    'name' => Craft::t('formie', 'First Name'),
+                    'required' => true,
+                ]),
+                new IntegrationField([
+                    'handle' => 'middle_name',
+                    'name' => Craft::t('formie', 'Middle Name'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'last_name',
+                    'name' => Craft::t('formie', 'Last Name'),
+                    'required' => true,
+                ]),
+                new IntegrationField([
+                    'handle' => 'phone1',
+                    'name' => Craft::t('formie', 'Phone 1'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'phone2',
+                    'name' => Craft::t('formie', 'Phone 2'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'position',
+                    'name' => Craft::t('formie', 'Position'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'quick_account_name',
+                    'name' => Craft::t('formie', 'Quick Account Name'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'title',
+                    'name' => Craft::t('formie', 'Fitle'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'account_position',
+                    'name' => Craft::t('formie', 'Account Position'),
+                ]),
+                new IntegrationField([
+                    'handle' => 'is_unsubscribed',
+                    'name' => Craft::t('formie', 'Is Unsubscribed'),
+                ]),
+            ];
+
+            $settings = [
+                'contact' => $contactFields,
+            ];
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
                 'message' => $e->getMessage(),
@@ -71,7 +183,7 @@ class Pipeliner extends Crm
             ]), true);
         }
 
-        return $allLists;
+        return $settings;
     }
 
     /**
@@ -80,7 +192,25 @@ class Pipeliner extends Crm
     public function sendPayload(Submission $submission): bool
     {
         try {
-            
+            $contactValues = $this->getFieldMappingValues($submission, $this->contactFieldMapping);
+
+            $contactPayload = $contactValues;
+
+            $response = $this->deliverPayload($submission, 'entities/Contacts', $contactPayload);
+
+            if ($response === false) {
+                return false;
+            }
+
+            $contactId = $response['data']['id'] ?? '';
+
+            if (!$contactId) {
+                Integration::error($this, Craft::t('formie', 'Missing return “contactId” {response}', [
+                    'response' => Json::encode($response),
+                ]), true);
+
+                return false;
+            }
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
                 'message' => $e->getMessage(),
@@ -100,7 +230,7 @@ class Pipeliner extends Crm
     public function fetchConnection(): bool
     {
         try {
-            
+            $response = $this->request('GET', 'entities/Contacts');
         } catch (\Throwable $e) {
             Integration::error($this, Craft::t('formie', 'API error: “{message}” {file}:{line}', [
                 'message' => $e->getMessage(),
@@ -128,8 +258,33 @@ class Pipeliner extends Crm
         }
 
         return $this->_client = Craft::createGuzzleClient([
-            'base_uri' => '',
-            'headers' => ['Api-Token' => $this->apiKey],
+            'base_uri' => "{$this->apiServiceUrl}/api/v100/rest/spaces/{$this->apiSpaceId}/",
+            'auth' => [$this->apiToken, $this->apiPassword],
         ]);
+    }
+
+
+    // Private Methods
+    // =========================================================================
+
+    /**
+     * @inheritDoc
+     */
+    private function _getCustomFields($fields, $excludeNames = [])
+    {
+        $customFields = [];
+
+        foreach ($fields as $key => $field) {
+            if ($field['system_readonly']) {
+                continue;
+            }
+
+            $customFields[] = new IntegrationField([
+                'handle' => $field['name'],
+                'name' => $field['translated_name'],
+            ]);
+        }
+
+        return $customFields;
     }
 }
