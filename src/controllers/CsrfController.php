ae4d74e0d6eedbb075a5ac10ff14f6dd2b8c1cd2
Josh Crawford <joshua@sgroup.com.au>
2020-09-10 21:38:38 +1000

    Add CSRF controller

---
+++ src/controllers/CsrfController.php
@@ -0,0 +1,55 @@
+<?php
+namespace verbb\formie\controllers;
+
+use Craft;
+use craft\web\Controller;
+
+use yii\web\Response;
+
+class CsrfController extends Controller
+{
+    // Properties
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    protected $allowAnonymous = true;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Returns a CSRF input field.
+     *
+     * @return Response
+     */
+    public function actionInput(): Response
+    {
+        $request = Craft::$app->getRequest();
+
+        $input = '<input type="hidden" name="' . $request->csrfParam . '" value="' . $request->getCsrfToken() . '">';
+
+        return $this->asRaw($input);
+    }
+
+    /**
+     * Returns the CSRF param.
+     *
+     * @return Response
+     */
+    public function actionParam(): Response
+    {
+        return $this->asRaw(Craft::$app->getRequest()->csrfParam);
+    }
+
+    /**
+     * Returns a CSRF token.
+     *
+     * @return Response
+     */
+    public function actionToken(): Response
+    {
+        return $this->asRaw(Craft::$app->getRequest()->getCsrfToken());
+    }
+}
