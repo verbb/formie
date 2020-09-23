d136e8a5e081b86af0cab0d9c260b75addbdde5b
Josh Crawford <joshua@sgroup.com.au>
2020-09-23 19:41:59 +1000

    Fix `isJsonObject` error for 3.4

--- src/fields/formfields/Recipients.php
+++ src/fields/formfields/Recipients.php
@@ -43,6 +43,15 @@ class Recipients extends FormField
         return 'formie/_formfields/recipients/icon.svg';
     }
 
+    /**
+     * Returns whether a string value looks like a JSON object or array.
+     * TODO: Remove this when bumping to 3.5+
+     */
+    public static function isJsonObject(string $str): bool
+    {
+        return (bool)preg_match('/^(?:\{.*\}|\[.*\])$/s', $str);
+    }
+
 
     // Public Methods
     // =========================================================================
@@ -69,7 +78,7 @@ class Recipients extends FormField
     public function normalizeValue($value, ElementInterface $element = null)
     {
         // Sort out multiple options being set
-        if (is_string($value) && Json::isJsonObject($value)) {
+        if (is_string($value) && self::isJsonObject($value)) {
             $value = implode(',', array_filter(Json::decode($value)));
         }
 
@@ -281,7 +290,7 @@ class Recipients extends FormField
             $value = StringHelper::decdec($value);
 
             // Check if this was an array of data
-            if (is_string($value) && Json::isJsonObject($value)) {
+            if (is_string($value) && self::isJsonObject($value)) {
                 $value = implode(',', array_filter(Json::decode($value)));
             }
         }
