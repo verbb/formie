94e48d54146949fc406bc9c9edc1a7af1a6e687b
Josh Crawford <joshua@sgroup.com.au>
2020-10-12 10:39:33 +1100

    Allow form includes to be overridden individually

--- src/web/twig/Extension.php
+++ src/web/twig/Extension.php
@@ -4,9 +4,13 @@ namespace verbb\formie\web\twig;
 use verbb\formie\Formie;
 use verbb\formie\helpers\RichTextHelper;
 
+use Craft;
+use craft\web\View;
+
 use Twig_Extension;
 use Twig_SimpleFunction;
 use Twig_SimpleFilter;
+use Twig_Environment;
 
 class Extension extends Twig_Extension
 {
@@ -22,6 +26,22 @@ class Extension extends Twig_Extension
     {
         return [
             new Twig_SimpleFunction('getRichTextConfig', [new RichTextHelper(), 'getRichTextConfig']),
+            new Twig_SimpleFunction('formieInclude', [$this, 'formieInclude'], ['needs_environment' => true, 'needs_context' => true, 'is_safe' => ['all']]),
         ];
+    }
+
+    public function formieInclude(Twig_Environment $env, $context, $template, $variables = [], $withContext = true, $ignoreMissing = false, $sandboxed = false) {
+        // Get the form from the context
+        $form = $context['form'] ?? null;
+
+        if ($form) {
+            $view = $context['view'];
+
+            // Render the provided include depending on form template overrides
+            $templatePath = Formie::$plugin->getRendering()->getFormComponentTemplatePath($form, $template);
+            $view->setTemplatesPath($templatePath);
+        }
+        
+        return twig_include($env, $context, $template, $variables, $withContext, $ignoreMissing, $sandboxed);
     }
 }
