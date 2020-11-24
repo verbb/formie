9460ce31ec73ae931b8d65ac7512460ffc5714a4
Josh Crawford <joshua@sgroup.com.au>
2020-11-24 20:49:34 +1100

    Fix agree field description not outputting line breaks

--- src/fields/formfields/Agree.php
+++ src/fields/formfields/Agree.php
@@ -218,8 +218,9 @@ class Agree extends FormField implements PreviewableFieldInterface
             'content' => $content,
         ]);
 
-        // Strip out paragraphs
-        $html = str_replace(['<p>', '</p>'], ['', ''], $html);
+        // Strip out paragraphs, replace with `<br>`
+        $html = str_replace(['<p>', '</p>'], ['', '<br>'], $html);
+        $html = trim($html, '<br>');
 
         // Prosemirror will use `htmlentities` for special characters, but doesn't play nice
         // with static translations. Convert them back.
