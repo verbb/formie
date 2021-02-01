17d0515e608f370ff216bd892df0a612cba13b0f
Josh Crawford <joshua@sgroup.com.au>
2021-02-02 10:27:51 +1100

    Fixed Tags field not providing data to integrations correctly.

--- src/fields/formfields/Tags.php
+++ src/fields/formfields/Tags.php
@@ -146,6 +146,16 @@ class Tags extends CraftTags implements FormFieldInterface
     /**
      * @inheritDoc
      */
+    public function serializeValueForIntegration($value, ElementInterface $element = null)
+    {
+        return array_map(function($input) {
+            return $input->title;
+        }, $this->_all($value, $element)->all());
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function getExtraBaseFieldConfig(): array
     {
         $options = $this->getSourceOptions();
