23dc41ffe4a9f3cdaa2989b6c2816626064cb9f5
Josh Crawford <joshua@sgroup.com.au>
2021-02-02 10:27:28 +1100

    Fixed Variant field not providing data to integrations correctly.

--- src/fields/formfields/Variants.php
+++ src/fields/formfields/Variants.php
@@ -93,6 +93,16 @@ class Variants extends CommerceVariants implements FormFieldInterface
     /**
      * @inheritDoc
      */
+    public function serializeValueForIntegration($value, ElementInterface $element = null)
+    {
+        return array_map(function($input) {
+            return $input->title;
+        }, $this->_all($value, $element)->all());
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function getExtraBaseFieldConfig(): array
     {
         $options = $this->getSourceOptions();
