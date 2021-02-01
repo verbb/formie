e4102336c7f2d0ca3081a0a59b5cdb38d28d996d
Josh Crawford <joshua@sgroup.com.au>
2021-02-02 10:28:53 +1100

    Fix email notification conditions not properly testing against element field values (entries, categories, etc)

--- src/helpers/ConditionsHelper.php
+++ src/helpers/ConditionsHelper.php
@@ -12,28 +12,76 @@ class ConditionsHelper
     // Public Methods
     // =========================================================================
 
+    /**
+     * @inheritDoc
+     */
     public static function getRuler()
     {
         $ruler = new Ruler();
 
         $ruler->getDefaultAsserter()->setOperator('contains', function($subject, $pattern) {
+            if (is_array($subject)) {
+                $subject = self::recursiveImplode(' ', $subject);
+            }
+
             return StringHelper::contains($subject, $pattern);
         });
 
         $ruler->getDefaultAsserter()->setOperator('startswith', function($subject, $pattern) {
+            if (is_array($subject)) {
+                $subject = self::recursiveImplode(' ', $subject);
+            }
+
             return StringHelper::startsWith($subject, $pattern);
         });
 
         $ruler->getDefaultAsserter()->setOperator('endswith', function($subject, $pattern) {
+            if (is_array($subject)) {
+                $subject = self::recursiveImplode(' ', $subject);
+            }
+
             return StringHelper::endsWith($subject, $pattern);
         });
 
         return $ruler;
     }
 
+    /**
+     * @inheritDoc
+     */
     public static function getContext($conditions = [])
     {
         return new Context($conditions);
     }
 
+    /**
+     * Recursively implodes an array with optional key inclusion
+     * 
+     * Example of $include_keys output: key, value, key, value, key, value
+     * 
+     * @access  public
+     * @param   array   $array         multi-dimensional array to recursively implode
+     * @param   string  $glue          value that glues elements together   
+     * @param   bool    $include_keys  include keys before their values
+     * @param   bool    $trim_all      trim ALL whitespace from string
+     * @return  string  imploded array
+     */ 
+    public static function recursiveImplode($glue = ',', array $array, $include_keys = false, $trim_all = false)
+    {
+        $glued_string = '';
+
+        // Recursively iterates array and adds key/value to glued string
+        array_walk_recursive($array, function($value, $key) use ($glue, $include_keys, &$glued_string) {
+            $include_keys && $glued_string .= $key.$glue;
+            $glued_string .= $value.$glue;
+        });
+
+        // Removes last $glue from string
+        strlen($glue) > 0 && $glued_string = substr($glued_string, 0, -strlen($glue));
+
+        // Trim ALL whitespace
+        $trim_all && $glued_string = preg_replace("/(\s)/ixsm", '', $glued_string);
+
+        return (string)$glued_string;
+    }
 }
--- src/services/Notifications.php
+++ src/services/Notifications.php
@@ -415,7 +415,7 @@ class Notifications extends Component
                         }
 
                         // Protect against empty conditions
-                        if (!trim($this->recursiveImplode('', $condition))) {
+                        if (!trim(ConditionsHelper::recursiveImplode('', $condition))) {
                             continue;
                         }
 
@@ -425,7 +425,7 @@ class Notifications extends Component
                         $results[] = $ruler->assert($rule, $context);
                     } catch (\Throwable $e) {
                         Formie::error(Craft::t('formie', 'Failed to parse conditional “{rule}”: “{message}” {file}:{line}', [
-                            'rule' => trim($this->recursiveImplode('', $condition)),
+                            'rule' => trim(ConditionsHelper::recursiveImplode('', $condition)),
                             'message' => $e->getMessage(),
                             'file' => $e->getFile(),
                             'line' => $e->getLine(),
@@ -735,36 +735,5 @@ class Notifications extends Component
         }
 
         return new NotificationRecord();
-    }
-
-    /**
-     * Recursively implodes an array with optional key inclusion
-     * 
-     * Example of $include_keys output: key, value, key, value, key, value
-     * 
-     * @access  public
-     * @param   array   $array         multi-dimensional array to recursively implode
-     * @param   string  $glue          value that glues elements together   
-     * @param   bool    $include_keys  include keys before their values
-     * @param   bool    $trim_all      trim ALL whitespace from string
-     * @return  string  imploded array
-     */ 
-    private function recursiveImplode($glue = ',', array $array, $include_keys = false, $trim_all = true)
-    {
-        $glued_string = '';
-
-        // Recursively iterates array and adds key/value to glued string
-        array_walk_recursive($array, function($value, $key) use ($glue, $include_keys, &$glued_string) {
-            $include_keys && $glued_string .= $key.$glue;
-            $glued_string .= $value.$glue;
-        });
-
-        // Removes last $glue from string
-        strlen($glue) > 0 && $glued_string = substr($glued_string, 0, -strlen($glue));
-
-        // Trim ALL whitespace
-        $trim_all && $glued_string = preg_replace("/(\s)/ixsm", '', $glued_string);
-
-        return (string)$glued_string;
     }
 }
