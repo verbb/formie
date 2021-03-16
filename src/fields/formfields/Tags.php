ed230d264d0fa77969fca95eff137a4682448ce5
Josh Crawford <joshua@sgroup.com.au>
2021-03-16 11:27:03 +1100

    Fix Tags field default value and limit

--- src/fields/formfields/Tags.php
+++ src/fields/formfields/Tags.php
@@ -279,6 +279,22 @@ class Tags extends CraftTags implements FormFieldInterface
             $query->group($group);
         }
 
+        // Check if a default value has been set, and set it before limiting
+        if ($this->defaultValue) {
+            $ids = [];
+
+            // Handle the two ways a default value can be set
+            if ($this->defaultValue instanceof ElementQueryInterface) {
+                $ids = $this->defaultValue->id;
+            } else {
+                $ids = ArrayHelper::getColumn($this->defaultValue, 'id');
+            }
+            
+            if ($ids) {
+                $query->id($ids);
+            }
+        }
+
         $query->limit($this->limit);
 
         $query->orderBy('title ASC');
