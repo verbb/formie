d76075b853bb126b82d554e9bd672ef476c35d53
Josh Crawford <joshua@sgroup.com.au>
2021-04-15 21:01:17 +1000

    Remove duplicate “Pre-populate” field settings for Hidden fields

--- src/fields/formfields/Hidden.php
+++ src/fields/formfields/Hidden.php
@@ -207,7 +207,6 @@ class Hidden extends FormField implements PreviewableFieldInterface
     public function defineSettingsSchema(): array
     {
         return [
-            SchemaHelper::prePopulate(),
             SchemaHelper::lightswitchField([
                 'label' => Craft::t('formie', 'Include in Email Notifications'),
                 'help' => Craft::t('formie', 'Whether the content of this field should be included in email notifications.'),
