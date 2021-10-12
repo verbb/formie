d23013652913037cbb6d41a25cd2d3f3673607f4
Josh Crawford <joshua@sgroup.com.au>
2021-10-12 14:10:19 +1100

    Fix Table fields inside a Group field not saving correctly

--- src/variables/Formie.php
+++ src/variables/Formie.php
@@ -322,4 +322,21 @@ class Formie
     {
         return FormiePlugin::$plugin->getRelations()->getSubmissionRelations($element);
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFieldNamespaceForScript($field)
+    {
+        $scriptNamespace = null;
+        $currentNamespace = Craft::$app->getView()->getNamespace();
+
+        $namespace = $field->namespace == null ? $field->handle : "{$field->namespace}[{$field->handle}]";
+
+        if ($currentNamespace) {
+            $scriptNamespace = Craft::$app->getView()->namespaceInputName($currentNamespace, $field->namespace);
+        }
+
+        return Craft::$app->getView()->namespaceInputName($namespace, $scriptNamespace);
+    }
 }
