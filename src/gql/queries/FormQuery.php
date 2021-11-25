cae2f43eef5a99c1483ad9d289b9e40b4d202cd7
Josh Crawford <joshua@sgroup.com.au>
2021-11-26 08:46:14 +1100

    Add `formCount` and `submissionCount` to GraphQL queries for forms and submissions

--- src/gql/queries/FormQuery.php
+++ src/gql/queries/FormQuery.php
@@ -27,24 +27,42 @@ class FormQuery extends Query
                 'args' => FormArguments::getArguments(),
                 'resolve' => FormResolver::class . '::resolve',
                 'description' => 'This query is used to query for forms.',
+                'complexity' => GqlHelper::relatedArgumentComplexity(),
             ],
             'formieForm' => [
                 'type' => FormInterface::getType(),
                 'args' => FormArguments::getArguments(),
                 'resolve' => FormResolver::class . '::resolveOne',
                 'description' => 'This query is used to query for a single form.',
+                'complexity' => GqlHelper::singleQueryComplexity(),
+            ],
+            'formieFormCount' => [
+                'type' => Type::nonNull(Type::int()),
+                'args' => FormArguments::getArguments(),
+                'resolve' => FormResolver::class . '::resolveCount',
+                'description' => 'This query is used to return the number of forms.',
+                'complexity' => GqlHelper::singleQueryComplexity(),
             ],
             'forms' => [
                 'type' => Type::listOf(FormInterface::getType()),
                 'args' => FormArguments::getArguments(),
                 'resolve' => FormResolver::class . '::resolve',
                 'description' => 'This query is used to query for forms.',
+                'complexity' => GqlHelper::relatedArgumentComplexity(),
             ],
             'form' => [
                 'type' => FormInterface::getType(),
                 'args' => FormArguments::getArguments(),
                 'resolve' => FormResolver::class . '::resolveOne',
                 'description' => 'This query is used to query for a single form.',
+                'complexity' => GqlHelper::singleQueryComplexity(),
+            ],
+            'formCount' => [
+                'type' => Type::nonNull(Type::int()),
+                'args' => FormArguments::getArguments(),
+                'resolve' => FormResolver::class . '::resolveCount',
+                'description' => 'This query is used to return the number of forms.',
+                'complexity' => GqlHelper::singleQueryComplexity(),
             ],
         ];
     }
