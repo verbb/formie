8aefc65b28a2e3698f4fe8076b9f34f3adfce090
Josh Crawford <joshua@sgroup.com.au>
2022-03-10 20:08:06 +1100

    Remove unneeded migrations

--- src/Formie.php
+++ src/Formie.php
@@ -88,6 +88,7 @@ class Formie extends Plugin
     public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
     public string $schemaVersion = '1.2.7';
+    public string $minVersionRequired = '1.2.7';
 
 
     // Traits
