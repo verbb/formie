cf0415ed3908913604722138e43e7282d57fa132
Josh Crawford <joshua@sgroup.com.au>
2022-06-01 14:27:02 +1000

    Add ability to query submissions across multiple forms via GraphQL

--- src/gql/resolvers/SubmissionResolver.php
+++ src/gql/resolvers/SubmissionResolver.php
@@ -4,8 +4,12 @@ namespace verbb\formie\gql\resolvers;
 use verbb\formie\elements\Submission;
 use verbb\formie\helpers\Gql as GqlHelper;
 
+use craft\elements\db\ElementQuery;
 use craft\gql\base\ElementResolver;
 use craft\helpers\Db;
+
+use GraphQL\Language\AST\InlineFragmentNode;
+use GraphQL\Type\Definition\ResolveInfo;
 
 class SubmissionResolver extends ElementResolver
 {
@@ -39,5 +43,37 @@ class SubmissionResolver extends ElementResolver
         }
 
         return $query;
+    }
+
+    public static function resolve(mixed $source, array $arguments, mixed $context, ResolveInfo $resolveInfo): mixed
+    {
+        $query = self::prepareElementQuery($source, $arguments, $context, $resolveInfo);
+
+        // Try and automatically set the submissions' context based on the inline fragment used. This is because submissions
+        // require a form context to resolve their custom field values, and sometimes, we don't want to supply the "form:handle"
+        // GQL query param for `formieSubmissions`. Instead, because we already use inline fragments (`...on contactForm_Submission`)
+        // We can make use of that, and set the form param on the Submission element query.
+        // Unfortunately, we don't have access to `$resolveInfo` in `prepareQuery()`.
+        foreach ($resolveInfo->fieldNodes as $fieldNode) {
+            if ($fieldNode->selectionSet === null) {
+                continue;
+            }
+
+            if ($fieldNode->selectionSet) {
+                foreach ($fieldNode->selectionSet->selections as $selectionNode) {
+                    if ($selectionNode instanceof InlineFragmentNode) {
+                        $fragmentName = $selectionNode->typeCondition->name->value ?? '';
+
+                        if ($fragmentName && strstr($fragmentName, '_Submission')) {
+                            $query->form(explode('_', $fragmentName)[0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        $value = $query instanceof ElementQuery ? $query->all() : $query;
+
+        return GqlHelper::applyDirectives($source, $resolveInfo, $value);
     }
 }
