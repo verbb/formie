c3b85baeac2e436cee88f9d6f0964106f5d311d2
Josh Crawford <joshua@sgroup.com.au>
2022-08-20 11:16:09 +1000

    Add more logging to Salesforce integration with regards to duplicate lead task-creation

--- src/integrations/crm/Salesforce.php
+++ src/integrations/crm/Salesforce.php
@@ -401,6 +401,7 @@ class Salesforce extends Crm
                     }
                 } catch (\Throwable $e) {
                     // Ignore duplicate warnings and continue, but still log
+                    Integration::error($this, 'Duplicate lead found.', false);
                     Integration::apiError($this, $e, false);
 
                     // Check if we should enable tasks to be created for duplicates
@@ -408,6 +409,8 @@ class Salesforce extends Crm
                     $responseCode = $response[0]['errorCode'] ?? '';
 
                     if ($responseCode === 'DUPLICATES_DETECTED' && $this->duplicateLeadTask) {
+                        Integration::error($this, 'Attempting to create task for duplicate lead.', false);
+
                         $taskPayload = [
                             'Subject' => $this->duplicateLeadTaskSubject,
                             'WhoId' => $contactId,
@@ -424,8 +427,15 @@ class Salesforce extends Crm
                             if ($response === false) {
                                 return true;
                             }
+
+                            Integration::log($this, Craft::t('formie', 'Response from task-creation {response}. Sent payload {payload}', [
+                                'response' => Json::encode($response),
+                                'payload' => Json::encode($taskPayload),
+                            ]));
                         } catch (\Throwable $e) {
-                            Integration::apiError($this, $e, false);
+                            Integration::apiError($this, $e);
+
+                            return false;
                         }
                     }
                 }
