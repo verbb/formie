cc6c9e687645ac8efa2e9014f6a3910fb36f1547
Josh Crawford <joshua@sgroup.com.au>
2022-09-21 08:25:23 +1000

    Fix progress bar not working correctly when switch tabs on Ajax-based forms

--- src/web/assets/frontend/src/js/formie-form-theme.js
+++ src/web/assets/frontend/src/js/formie-form-theme.js
@@ -753,7 +753,7 @@ export class FormieFormTheme {
         }
 
         // Update tabs and progress bar if we're using them
-        const $progress = this.$form.querySelector('[data-fui-progress]');
+        const $progress = this.$form.querySelector('[data-fui-progress-bar]');
 
         if ($progress && data.nextPageIndex) {
             const pageIndex = parseInt(data.nextPageIndex, 10) + 1;
