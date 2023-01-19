82eda501db45b7cfdfa5f90288e7018ef0da38bf
Josh Crawford <joshua@sgroup.com.au>
2023-01-20 00:18:15 +1100

    Fix Honeypot spam captcha checks

--- src/integrations/captchas/Honeypot.php
+++ src/integrations/captchas/Honeypot.php
@@ -63,7 +63,8 @@ class Honeypot extends Captcha
         }
 
         // If the honeypot param has been stripped out of the request altogether
-        if ($this->getRequestParam(self::HONEYPOT_INPUT_NAME) === null) {
+        // Don't use `getRequestParam()` as we want to check for strict `null`
+        if (Craft::$app->getRequest()->getParam(self::HONEYPOT_INPUT_NAME) === null) {
             $this->spamReason = Craft::t('formie', 'Honeypot param missing: {v}.', ['v' => self::HONEYPOT_INPUT_NAME]);
 
             return false;
