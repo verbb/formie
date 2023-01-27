72add4992311be902a412e5cde2db779527bce05
James White <james@jmwhite.co.uk>
2023-01-27 18:12:46 +0000

    Various Microsoft Dynamics 365 CRM integration improvements and optimisations Resolves #1250, #1262, #1265, #1276, #1277

---
+++ src/events/MicrosoftDynamics365RequiredLevelsEvent.php
@@ -0,0 +1,13 @@
+<?php
+namespace verbb\formie\events;
+
+use yii\base\Event;
+
+class MicrosoftDynamics365RequiredLevelsEvent extends Event
+{
+    // Properties
+    // =========================================================================
+
+    public array $requiredLevels;
+
+}
