f66e246fc8f049fe518ab4ebf06887e85fb2f19a
Josh Crawford <joshua@sgroup.com.au>
2023-01-31 14:58:17 +1100

    Fix a type error for `PdfRenderOptionsEvent`

--- src/events/PdfRenderOptionsEvent.php
+++ src/events/PdfRenderOptionsEvent.php
@@ -3,7 +3,9 @@ namespace verbb\formie\events;
 
 use yii\base\Event;
 
+use Dompdf\Options;
+
 class PdfRenderOptionsEvent extends Event
 {
-    public ?array $options;
+    public Options $options;
 }
