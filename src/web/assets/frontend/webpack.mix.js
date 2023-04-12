975389975c4fd33774c40a1c6e78cc45314e2be1
Josh Crawford <joshua@sgroup.com.au>
2023-04-12 22:26:19 +1000

    assets

--- src/web/assets/frontend/webpack.mix.js
+++ src/web/assets/frontend/webpack.mix.js
@@ -40,6 +40,7 @@ mix.js('./src/js/captchas/recaptcha-enterprise.js', 'js/captchas');
 mix.js('./src/js/captchas/javascript.js', 'js/captchas');
 mix.js('./src/js/captchas/hcaptcha.js', 'js/captchas');
 mix.js('./src/js/captchas/friendly-captcha.js', 'js/captchas');
+mix.js('./src/js/captchas/turnstile.js', 'js/captchas');
 mix.js('./src/js/payments/stripe.js', 'js/payments');
 mix.js('./src/js/payments/paypal.js', 'js/payments');
 mix.js('./src/js/payments/payway.js', 'js/payments');
