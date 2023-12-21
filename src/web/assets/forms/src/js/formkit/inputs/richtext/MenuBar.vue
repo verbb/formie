2a20f6e4868217cea267a0a2d4bf6dc1ef421b37
Josh Crawford <joshua@sgroup.com.au>
2023-12-21 23:02:46 +1100

    Fix formatting buttons not appearing at the top-level of a rich text editor instance

--- src/web/assets/forms/src/js/formkit/inputs/richtext/MenuBar.vue
+++ src/web/assets/forms/src/js/formkit/inputs/richtext/MenuBar.vue
@@ -101,9 +101,10 @@ export default {
                 },
                 {
                     name: 'paragraph',
-                    icon: 'paragraph',
+                    svg: 'text',
                     title: Craft.t('formie', 'Paragraph'),
                     action: () => { return this.editor.chain().focus().setParagraph().run(); },
+                    isActive: () => { return this.editor.isActive('paragraph'); },
                 },
                 {
                     name: 'unordered-list',
@@ -127,17 +128,60 @@ export default {
                     isActive: () => { return this.editor.isActive('codeBlock'); },
                 },
                 {
-                    name: 'quote',
-                    icon: 'quote-right',
+                    name: 'blockquote',
+                    svg: 'quote-right',
                     title: Craft.t('formie', 'Blockquote'),
                     action: () => { return this.editor.chain().focus().toggleBlockquote().run(); },
                     isActive: () => { return this.editor.isActive('blockquote'); },
+                },
+                {
+                    name: 'h1',
+                    svg: 'h1',
+                    title: Craft.t('formie', 'Heading 1'),
+                    action: () => { return this.editor.chain().focus().toggleHeading({ level: 1 }).run(); },
+                    isActive: () => { return this.editor.isActive('heading', { level: 1 }); },
+                },
+                {
+                    name: 'h2',
+                    svg: 'h2',
+                    title: Craft.t('formie', 'Heading 2'),
+                    action: () => { return this.editor.chain().focus().toggleHeading({ level: 2 }).run(); },
+                    isActive: () => { return this.editor.isActive('heading', { level: 2 }); },
+                },
+                {
+                    name: 'h3',
+                    svg: 'h3',
+                    title: Craft.t('formie', 'Heading 3'),
+                    action: () => { return this.editor.chain().focus().toggleHeading({ level: 3 }).run(); },
+                    isActive: () => { return this.editor.isActive('heading', { level: 3 }); },
+                },
+                {
+                    name: 'h4',
+                    svg: 'h4',
+                    title: Craft.t('formie', 'Heading 4'),
+                    action: () => { return this.editor.chain().focus().toggleHeading({ level: 4 }).run(); },
+                    isActive: () => { return this.editor.isActive('heading', { level: 4 }); },
+                },
+                {
+                    name: 'h5',
+                    svg: 'h5',
+                    title: Craft.t('formie', 'Heading 5'),
+                    action: () => { return this.editor.chain().focus().toggleHeading({ level: 5 }).run(); },
+                    isActive: () => { return this.editor.isActive('heading', { level: 5 }); },
+                },
+                {
+                    name: 'h6',
+                    svg: 'h6',
+                    title: Craft.t('formie', 'Heading 6'),
+                    action: () => { return this.editor.chain().focus().toggleHeading({ level: 6 }).run(); },
+                    isActive: () => { return this.editor.isActive('heading', { level: 6 }); },
                 },
                 {
                     name: 'hr',
                     svg: 'horizontal-rule',
                     title: Craft.t('formie', 'Horizontal Rule'),
                     action: () => { return this.editor.chain().focus().setHorizontalRule().run(); },
+                    isActive: () => { return this.editor.isActive('hr'); },
                 },
                 {
                     name: 'line-break',
@@ -168,24 +212,28 @@ export default {
                     icon: 'align-left',
                     title: Craft.t('formie', 'Align Left'),
                     action: () => { return this.editor.chain().focus().setTextAlign('left').run(); },
+                    isActive: () => { return this.editor.isActive({ textAlign: 'left' }); },
                 },
                 {
                     name: 'align-center',
                     icon: 'align-center',
                     title: Craft.t('formie', 'Align Center'),
                     action: () => { return this.editor.chain().focus().setTextAlign('center').run(); },
+                    isActive: () => { return this.editor.isActive({ textAlign: 'center' }); },
                 },
                 {
                     name: 'align-right',
                     icon: 'align-right',
                     title: Craft.t('formie', 'Align Right'),
                     action: () => { return this.editor.chain().focus().setTextAlign('right').run(); },
+                    isActive: () => { return this.editor.isActive({ textAlign: 'right' }); },
                 },
                 {
                     name: 'align-justify',
                     icon: 'align-justify',
                     title: Craft.t('formie', 'Align Justify'),
                     action: () => { return this.editor.chain().focus().setTextAlign('justify').run(); },
+                    isActive: () => { return this.editor.isActive({ textAlign: 'justify' }); },
                 },
                 {
                     name: 'formatting',
@@ -195,46 +243,129 @@ export default {
                         {
                             name: 'paragraph',
                             title: Craft.t('formie', 'Paragraph'),
+                            action: () => { return this.editor.chain().focus().setParagraph().run(); },
+                            isActive: () => { return this.editor.isActive('paragraph'); },
                         },
                         {
                             name: 'code-block',
                             title: Craft.t('formie', 'Code Block'),
                             action: () => { return this.editor.chain().focus().toggleCodeBlock().run(); },
+                            isActive: () => { return this.editor.isActive('codeBlock'); },
                         },
                         {
                             name: 'blockquote',
                             title: Craft.t('formie', 'Blockquote'),
                             action: () => { return this.editor.chain().focus().toggleBlockquote().run(); },
+                            isActive: () => { return this.editor.isActive('blockquote'); },
                         },
                         {
                             name: 'h1',
                             title: Craft.t('formie', 'Heading 1'),
                             action: () => { return this.editor.chain().focus().toggleHeading({ level: 1 }).run(); },
+                            isActive: () => { return this.editor.isActive('heading', { level: 1 }); },
                         },
                         {
                             name: 'h2',
                             title: Craft.t('formie', 'Heading 2'),
                             action: () => { return this.editor.chain().focus().toggleHeading({ level: 2 }).run(); },
+                            isActive: () => { return this.editor.isActive('heading', { level: 2 }); },
                         },
                         {
                             name: 'h3',
                             title: Craft.t('formie', 'Heading 3'),
                             action: () => { return this.editor.chain().focus().toggleHeading({ level: 3 }).run(); },
+                            isActive: () => { return this.editor.isActive('heading', { level: 3 }); },
                         },
                         {
                             name: 'h4',
                             title: Craft.t('formie', 'Heading 4'),
                             action: () => { return this.editor.chain().focus().toggleHeading({ level: 4 }).run(); },
+                            isActive: () => { return this.editor.isActive('heading', { level: 4 }); },
                         },
                         {
                             name: 'h5',
                             title: Craft.t('formie', 'Heading 5'),
                             action: () => { return this.editor.chain().focus().toggleHeading({ level: 5 }).run(); },
+                            isActive: () => { return this.editor.isActive('heading', { level: 5 }); },
                         },
                         {
                             name: 'h6',
                             title: Craft.t('formie', 'Heading 6'),
                             action: () => { return this.editor.chain().focus().toggleHeading({ level: 6 }).run(); },
+                            isActive: () => { return this.editor.isActive('heading', { level: 6 }); },
+                        },
+                    ],
+                },
+                {
+                    name: 'table',
+                    svg: 'table',
+                    title: Craft.t('formie', 'Table'),
+                    isActive: () => { return this.editor.isActive('table'); },
+                    options: [
+                        {
+                            name: 'insert-table',
+                            title: Craft.t('formie', 'Insert Table'),
+                            action: () => { return this.editor.chain().focus().insertTable({ rows: 3, cols: 3, withHeaderRow: true }).run(); },
+                        },
+                        {
+                            name: 'delete-table',
+                            title: Craft.t('formie', 'Delete Table'),
+                            action: () => { return this.editor.chain().focus().deleteTable().run(); },
+                        },
+                        {
+                            name: 'add-col-before',
+                            title: Craft.t('formie', 'Add Column Before'),
+                            action: () => { return this.editor.chain().focus().addColumnBefore().run(); },
+                        },
+                        {
+                            name: 'add-col-after',
+                            title: Craft.t('formie', 'Add Column After'),
+                            action: () => { return this.editor.chain().focus().addColumnAfter().run(); },
+                        },
+                        {
+                            name: 'delete-col',
+                            title: Craft.t('formie', 'Delete Column'),
+                            action: () => { return this.editor.chain().focus().deleteColumn().run(); },
+                        },
+                        {
+                            name: 'add-row-before',
+                            title: Craft.t('formie', 'Add Row Before'),
+                            action: () => { return this.editor.chain().focus().addRowBefore().run(); },
+                        },
+                        {
+                            name: 'add-row-after',
+                            title: Craft.t('formie', 'Add Row After'),
+                            action: () => { return this.editor.chain().focus().addRowAfter().run(); },
+                        },
+                        {
+                            name: 'delete-row',
+                            title: Craft.t('formie', 'Delete Row'),
+                            action: () => { return this.editor.chain().focus().deleteRow().run(); },
+                        },
+                        {
+                            name: 'merge-cells',
+                            title: Craft.t('formie', 'Merge Cells'),
+                            action: () => { return this.editor.chain().focus().mergeCells().run(); },
+                        },
+                        {
+                            name: 'split-cells',
+                            title: Craft.t('formie', 'Split Cells'),
+                            action: () => { return this.editor.chain().focus().splitCell().run(); },
+                        },
+                        {
+                            name: 'toggle-header-column',
+                            title: Craft.t('formie', 'Toggle Header Column'),
+                            action: () => { return this.editor.chain().focus().toggleHeaderColumn().run(); },
+                        },
+                        {
+                            name: 'toggle-header-row',
+                            title: Craft.t('formie', 'Toggle Header Row'),
+                            action: () => { return this.editor.chain().focus().toggleHeaderRow().run(); },
+                        },
+                        {
+                            name: 'toggle-header-cell',
+                            title: Craft.t('formie', 'Toggle Header Cell'),
+                            action: () => { return this.editor.chain().focus().toggleHeaderCell().run(); },
                         },
                     ],
                 },
@@ -267,6 +398,10 @@ export default {
                     // Handle special-cases and sub-options. Maybe move to other components?
                     if (button.name === 'formatting') {
                         button.options = this.getEnabledOptions(button, this.field.getFormattingOptions());
+                    }
+
+                    if (button.name === 'table') {
+                        button.options = this.getEnabledOptions(button, this.field.getTableOptions());
                     }
 
                     buttons.push(button);
