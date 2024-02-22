59ffb4ec35659d2431c2cdff85990d0083e5d5d2
Tim Kelty <tim@craftcms.com>
2024-02-22 15:05:52 -0500

    Initial cloud compatibility

---
+++ src/web/assets/frontend/FrontendAsset.php
@@ -0,0 +1,26 @@
+<?php
+namespace verbb\formie\web\assets\frontend;
+
+use craft\web\AssetBundle;
+
+class FrontendAsset extends AssetBundle
+{
+    // Public Methods
+    // =========================================================================
+
+    public function init(): void
+    {
+        $this->sourcePath = '@verbb/formie/web/assets/frontend/dist';
+
+        $this->js = [
+            'js/formie.js',
+        ];
+
+        $this->css = [
+            'css/formie-base.css',
+            'css/formie-theme.css',
+        ];
+
+        parent::init();
+    }
+}
