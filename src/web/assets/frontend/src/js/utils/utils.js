fcdb7fcc720bb62b97a684cf90f04a8dfb3ec052
Josh Crawford <joshua@sgroup.com.au>
2024-02-28 09:52:54 +1100

    assets

--- src/web/assets/frontend/src/js/utils/utils.js
+++ src/web/assets/frontend/src/js/utils/utils.js
@@ -10,3 +10,16 @@ export const eventKey = function(eventName) {
     return eventName + '.' + Math.random();
 };
 
+export const t = function(string, replacements = {}) {
+    if (window.FormieTranslations) {
+        string = window.FormieTranslations[string] || string;
+    }
+
+    return string.replace(/{([a-zA-Z0-9]+)}/g, (match, p1) => {
+        if (replacements[p1]) {
+            return replacements[p1];
+        }
+
+        return match;
+    });
+};
