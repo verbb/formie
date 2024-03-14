9f2fc673012c16d63978f880b44b39d6e93f8962
Josh Crawford <joshua@sgroup.com.au>
2024-03-15 10:56:53 +1100

    Fix success/error/other form messages not including paragraph nodes

--- src/models/FormSettings.php
+++ src/models/FormSettings.php
@@ -337,6 +337,6 @@ class FormSettings extends Model
 
     private function _getHtmlContent($content, $submission = null): string
     {
-        return RichTextHelper::getHtmlContent($content, $submission);
+        return RichTextHelper::getHtmlContent($content, $submission, false);
     }
 }
