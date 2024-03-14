a8e9128e409a4e16e35b1011fcd321672fa8d81b
Josh Crawford <joshua@sgroup.com.au>
2024-03-15 00:11:34 +1100

    Fix a reactivity issue when editing notifications, causing values to not always save

--- src/web/assets/forms/src/js/components/NotificationEditModal.vue
+++ src/web/assets/forms/src/js/components/NotificationEditModal.vue
@@ -26,7 +26,7 @@
                 <div class="fui-modal-content" :style="{ height: (!mounted) ? '80%' : '' }">
                     <div v-if="!mounted" class="fui-loading fui-loading-lg" style="height: 100%;"></div>
 
-                    <FormKitForm v-if="mounted" ref="fieldForm" v-model="notification" @submit="submitHandler">
+                    <FormKitForm v-if="mounted" ref="fieldForm" :model-value="notification" @update:model-value="notification = $event" @submit="submitHandler">
                         <FormKitSchema :schema="fieldsSchema" />
                     </FormKitForm>
                 </div>
