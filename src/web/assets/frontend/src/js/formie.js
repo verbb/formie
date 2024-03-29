b8c4c3acd3bbb24917544ff2f42caea8de47eb4d
Josh Crawford <joshua@sgroup.com.au>
2024-03-30 00:48:09 +1100

    Fix front-end JS throwing an error is some circumstances

--- src/web/assets/frontend/src/js/formie.js
+++ src/web/assets/frontend/src/js/formie.js
@@ -6,7 +6,11 @@ import { Formie } from './formie-lib';
 window.Formie = new Formie();
 
 // Whether we want to initialize the forms automatically.
-const initForms = document.currentScript.hasAttribute('data-manual-init') ? false : true;
+let initForms = true;
+
+if (document.currentScript && document.currentScript.hasAttribute('data-manual-init')) {
+    initForms = false;
+}
 
 // Don't init forms until the document is ready, or the document already loaded
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event#checking_whether_loading_is_already_complete
