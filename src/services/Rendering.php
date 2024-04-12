9c158b95116242227f53f8051fa8a2fabb3aa4db
Josh Crawford <joshua@sgroup.com.au>
2024-04-12 20:33:23 +1000

    Fix outdated front-end translations string for validators

--- src/services/Rendering.php
+++ src/services/Rendering.php
@@ -283,6 +283,15 @@ class Rendering extends Component
     public function getFrontEndJsTranslations(): array
     {
         return $this->_getTranslatedStrings([
+            // Core validators
+            '{attribute} cannot be blank.',
+            '{attribute} is not a valid email address.',
+            '{attribute} is not a valid URL.',
+            '{attribute} is not a valid number.',
+            '{attribute} is not a valid format.',
+            '{name} must match {value}.',
+
+            // Custom validators
             'File {filename} must be smaller than {filesize} MB.',
             'File must be smaller than {filesize} MB.',
             'File must be larger than {filesize} MB.',
@@ -292,25 +301,7 @@ class Rendering extends Component
             '{startTag}{num}{endTag} word left',
             '{startTag}{num}{endTag} words left',
 
-            // Field validation messages
-            'This field is required.',
-            'Please select a value.',
-            'Please select at least one value.',
-            'Please fill out this field.',
-            'Please enter a valid email address.',
-            'Please enter a URL.',
-            'Please enter a number',
-            'Please match the following format: #rrggbb',
-            'Please use the YYYY-MM-DD format',
-            'Please use the 24-hour time format. Ex. 23:00',
-            'Please use the YYYY-MM format',
-            'Please match the requested format.',
-            'Please select a value that is no more than {max}.',
-            'Please select a value that is no less than {min}.',
-            'Please shorten this text to no more than {maxLength} characters. You are currently using {length} characters.',
-            'Please lengthen this text to {minLength} characters or more. You are currently using {length} characters.',
-            'There was an error with this field.',
-
+            // General
             'Unable to parse response `{e}`.',
             'Are you sure you want to leave?',
             'The request timed out.',
@@ -331,6 +322,10 @@ class Rendering extends Component
             'The request timed out.',
             'The request encountered a network error. Please try again.',
 
+            // Stripe
+            'Invalid amount.'
+            'Invalid currency.',
+            'Provide a value for “{label}” to proceed.',
         ]);
     }
 
