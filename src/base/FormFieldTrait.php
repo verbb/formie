8b83964f1a583a392807562181db32292a41063e
Josh Crawford <joshua@sgroup.com.au>
2024-06-13 11:47:43 +1000

    Fix default values for fields not being trimmed of whitespace

--- src/base/FormFieldTrait.php
+++ src/base/FormFieldTrait.php
@@ -725,6 +725,10 @@ trait FormFieldTrait
 
         $this->trigger(static::EVENT_MODIFY_DEFAULT_VALUE, $event);
 
+        if (is_string($event->value)) {
+            $event->value = trim($event->value);
+        }
+
         return $event->value;
     }
 
