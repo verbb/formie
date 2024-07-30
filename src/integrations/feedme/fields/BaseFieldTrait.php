5b95fc3cce3de20a715911bcdc921b9cbc77e0a1
Josh Crawford <joshua@sgroup.com.au>
2024-07-30 22:30:57 +1000

    Fix Feed Me integration support

--- src/integrations/feedme/fields/BaseFieldTrait.php
+++ src/integrations/feedme/fields/BaseFieldTrait.php
@@ -10,7 +10,7 @@ trait BaseFieldTrait
     {
         // Feed Me assumes all fields are available in the global scope, so we fix that here.
         // We could also submit a PR to fix this at some point...
-        $this->field = $this->element->getFieldLayout()->getFieldByHandle($this->fieldHandle);
+        $this->field = $this->element->getFieldByHandle($this->fieldHandle);
     }
 
     public function parseField(): mixed
