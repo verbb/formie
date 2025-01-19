d263c1d74662619b7f3f528dad2271aa21e0e107
Josh Crawford <joshua@sgroup.com.au>
2025-01-19 21:43:33 +1100

    Add “is visible” and “is hidden” field conditions

--- src/web/assets/forms/src/js/formkit/FieldConditions.vue
+++ src/web/assets/forms/src/js/formkit/FieldConditions.vue
@@ -111,6 +111,8 @@ export default {
                 { label: Craft.t('formie', 'contains'), value: 'contains' },
                 { label: Craft.t('formie', 'starts with'), value: 'startsWith' },
                 { label: Craft.t('formie', 'ends with'), value: 'endsWith' },
+                { label: Craft.t('formie', 'is visible'), value: 'visible' },
+                { label: Craft.t('formie', 'is hidden'), value: 'hidden' },
             ],
             settings: {},
             defaultSettings: {
