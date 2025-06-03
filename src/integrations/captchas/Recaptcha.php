6f64dac2136d8c5e80b05ca949b528da1c4eafbf
Josh Crawford <joshua@sgroup.com.au>
2025-06-03 19:56:16 +1000

    Fix Recaptcha success logging

--- src/integrations/captchas/Recaptcha.php
+++ src/integrations/captchas/Recaptcha.php
@@ -251,7 +251,7 @@ class Recaptcha extends Captcha
 
             Formie::error(Craft::t('formie', '{token}:{spamReason}', [
                 'token' => $responseToken,
-                'spamReason' => Json::encode($result),
+                'spamReason' => $this->spamReason,
             ]));
         }
 
