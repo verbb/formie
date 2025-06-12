c5a29ada77546ce37445cdae6cb5bd04c0c4d1af
Josh Crawford <joshua@sgroup.com.au>
2025-06-12 11:03:54 +1000

    Improve scroll-to-error behaviour for some browsers/front-end setups

--- src/web/assets/frontend/src/js/formie-form-theme.js
+++ src/web/assets/frontend/src/js/formie-form-theme.js
@@ -791,14 +791,10 @@ export class FormieFormTheme {
     }
 
     scrollToForm() {
-        // Check for scroll-padding-top or `scroll-margin-top`
-        const extraPadding = parseInt(getComputedStyle(document.documentElement).scrollPaddingTop) || 0;
-        const extraMargin = parseInt(getComputedStyle(document.documentElement).scrollMarginTop) || 0;
-
         // Because the form can be hidden, use the parent wrapper
-        window.scrollTo({
-            top: this.$form.parentNode.getBoundingClientRect().top + window.pageYOffset - 100 - extraPadding - extraMargin,
+        this.$form.parentNode.scrollIntoView({
             behavior: 'smooth',
+            block: 'start', // Align to the top of the viewport
         });
     }
 
