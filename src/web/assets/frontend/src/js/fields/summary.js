ceabba79d1da2dcd3f86b8e18a0a905254bea70b
Josh Crawford <joshua@sgroup.com.au>
2025-08-29 10:32:34 +1000

    Add `onFieldVisible` and `onFetchSummary` JS events for the Summary field

--- src/web/assets/frontend/src/js/fields/summary.js
+++ src/web/assets/frontend/src/js/fields/summary.js
@@ -45,6 +45,14 @@ export class FormieSummary {
             return;
         }
 
+        // Emit an "onFieldVisible" event
+        this.$field.dispatchEvent(new CustomEvent('onFieldVisible', {
+            bubbles: true,
+            detail: {
+                summary: this,
+            },
+        }));
+
         this.debouncedFetch();
     }
 
@@ -70,6 +78,15 @@ export class FormieSummary {
             if (xhr.status >= 200 && xhr.status < 300) {
                 $container.parentNode.innerHTML = xhr.responseText;
             }
+
+            // Emit an "onFetchSummary" event
+            this.$field.dispatchEvent(new CustomEvent('onFetchSummary', {
+                bubbles: true,
+                detail: {
+                    summary: this,
+                    data: xhr,
+                },
+            }));
         };
 
         const params = {
