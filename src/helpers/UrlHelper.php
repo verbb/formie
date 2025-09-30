23d0480833acf3e74462f113515d806b2ccac5d0
Josh Crawford <joshua@sgroup.com.au>
2025-10-01 09:35:00 +1000

    Fix CSRF handling for multi-site installs with sub-directory setups

--- src/helpers/UrlHelper.php
+++ src/helpers/UrlHelper.php
@@ -14,10 +14,12 @@ class UrlHelper extends CraftUrlHelper
     public static function siteActionUrl(string $path = '', array|string|null $params = null, ?string $scheme = null, ?bool $showScriptName = null): string
     {
         // Swap the domain to resolve to the current site for front-end requests.
+        // Note that this should only be done for other domains, hence the check for host changes.
+        // Otherwise, sub-directory installs would be affected.
         // https://github.com/verbb/formie/issues/2479
         $url = static::actionUrl($path, $params, $scheme, $showScriptName);
-        $baseSiteUrl = rtrim(static::baseSiteUrl(), '/');
-        $baseCpUrl = rtrim(static::baseCpUrl(), '/');
+        $baseSiteUrl = parse_url(static::baseSiteUrl())['host'] ?? '';
+        $baseCpUrl = parse_url(static::baseCpUrl())['host'] ?? '';
 
         return str_replace($baseCpUrl, $baseSiteUrl, $url);
     }
