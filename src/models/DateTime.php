f77e6657b8aec89fda027052530a88e46484a5e3
Josh Crawford <joshua@sgroup.com.au>
2026-01-15 11:09:14 +1100

    Fix an error when using Date/Time fields and populating or setting default values for calendar displays

--- src/models/DateTime.php
+++ src/models/DateTime.php
@@ -3,41 +3,71 @@ namespace verbb\formie\models;
 
 use verbb\formie\base\FieldValueInterface;
 
-class DateTime extends \DateTime implements FieldValueInterface
+use DateTime as NativeDateTime;
+use DateTimeZone as NativeDateTimeZone;
+
+class DateTime extends NativeDateTime implements FieldValueInterface, \ArrayAccess
 {
+    // Properties
+    // =========================================================================
+
+    public const STORAGE_TZ = 'UTC';
+    
+
     // Public Methods
     // =========================================================================
 
+    public function __construct(string $datetime = 'now', ?NativeDateTimeZone $timezone = null)
+    {
+        // If no TZ provided, assume UTC
+        $timezone ??= new NativeDateTimeZone(self::STORAGE_TZ);
+
+        parent::__construct($datetime, $timezone);
+
+        // Normalize internal TZ to canonical
+        $this->setTimezone(new NativeDateTimeZone(self::STORAGE_TZ));
+    }
+
     public function __get(string $name): mixed
     {
-        if ($name === 'year') {
-            return (int)$this->format('Y');
-        }
+        return match ($name) {
+            'year' => (int)$this->format('Y'),
+            'month' => (int)$this->format('m'),
+            'day' => (int)$this->format('d'),
+            'hour' => (int)$this->format('H'),
+            'minute' => (int)$this->format('i'),
+            'second' => (int)$this->format('s'),
+            'ampm' => $this->format('A'),
+            default  => null,
+        };
+    }
 
-        if ($name === 'month') {
-            return (int)$this->format('m');
-        }
+    public function offsetExists(mixed $offset): bool
+    {
+        return in_array($offset, [
+            'year',
+            'month',
+            'day',
+            'hour',
+            'minute',
+            'second',
+            'ampm',
+        ], true);
+    }
 
-        if ($name === 'day') {
-            return (int)$this->format('d');
-        }
+    public function offsetGet(mixed $offset): mixed
+    {
+        // Delegate to __get() so logic stays in one place
+        return $this->__get((string)$offset);
+    }
 
-        if ($name === 'hour') {
-            return (int)$this->format('H');
-        }
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        throw new \LogicException('DateTime values are read-only.');
+    }
 
-        if ($name === 'minute') {
-            return (int)$this->format('i');
-        }
-
-        if ($name === 'second') {
-            return (int)$this->format('s');
-        }
-
-        if ($name === 'ampm') {
-            return $this->format('A');
-        }
-
-        return null;
+    public function offsetUnset(mixed $offset): void
+    {
+        throw new \LogicException('DateTime values are read-only.');
     }
 }
