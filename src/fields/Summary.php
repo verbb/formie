c121a318c00f00deaaaf3c8048f7d24d72d48732
Josh Crawford <joshua@sgroup.com.au>
2026-02-20 22:45:24 +1100

    Fix an issue with Summary fields not always generating a valid handle

--- src/fields/Summary.php
+++ src/fields/Summary.php
@@ -61,8 +61,8 @@ class Summary extends CosmeticField
 
     public function afterCreateField(array $data): void
     {
-        $this->label = $this->label ?? StringHelper::appendUniqueIdentifier(Craft::t('formie', 'Summary '));
-        $this->handle = $this->handle ?? StringHelper::appendUniqueIdentifier('summaryHandle');
+        $this->label = $this->label ?? StringHelper::appendRandomString(Craft::t('formie', 'Summary '), 15);
+        $this->handle = $this->handle ?? StringHelper::appendRandomString('summaryHandle', 15);
     }
 
     public function defineGeneralSchema(): array
